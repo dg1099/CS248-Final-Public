d075575c77912941364a41fe1f9bd9e4e9d97f2c
RuthPer <rp104@wellesley.edu>
2025-04-30 22:12:05 -0400

    asdasd

--- pages/Food Journal.py
+++ pages/Food Journal.py
@@ -318,7 +318,7 @@ else:
 
         # Fetch and print results
         results = cursor.fetchall()
-        cols1,cols2,cols3=st.columns(3)
+        cols1,cols2,cols3=st.columns([3,7])
         for (name,emotion,comment,meal) in results:
             with cols1:
                 st.write("💕")
